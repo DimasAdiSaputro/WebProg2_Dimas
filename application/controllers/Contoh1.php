640922288ed72dff8479781b5dab083e63e2c25f
DimasAdiSaputro <dimasadisaputr0699@gmail.com>
2022-03-16 21:44:24 +0700

    add codeigniter structure

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Hallo Dunia</h1>";
+        echo "Perkenalkan Nama Saya Dimas Adi Saputro, tinggal dijakut";
+    }
+}
