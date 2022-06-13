af21b6e29d96ca024a3b8d9886636d1dbe8adad0
DimasAdiSaputro <dimasadisaputr0699@gmail.com>
2022-06-13 23:32:48 +0700

    add Update ubah-profile

--- application/views/user/ubah-profile.php
+++ application/views/user/ubah-profile.php
@@ -3,7 +3,7 @@
 
     <div class="row">
         <div class="col-lg-9">
-            <?= form_open_multipart('user/ubahprofil'); ?>
+            <?= form_open_multipart('user/ubah-profile'); ?>
             <div class="form-group row">
                 <label for="email" class="col-sm-2 col-form-label">Email</label>
                 <div class="col-sm-10">
